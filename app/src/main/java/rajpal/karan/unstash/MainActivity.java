c5c4de90630c2259acb692db8a4bb79aac5f8083
karanrajpal14 <rajpal.karan73@gmail.com>
2017-07-30 18:41:38 +0530

    Added basic logging to main activity

--- app/src/main/java/rajpal/karan/unstash/MainActivity.java
+++ app/src/main/java/rajpal/karan/unstash/MainActivity.java
@@ -1,7 +1,10 @@
 package rajpal.karan.unstash;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,33 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		if (BuildConfig.DEBUG) {
+			Timber.plant(new Timber.DebugTree());
+		} else {
+			Timber.plant(new CrashReportingTree());
+
+		}
+
 	}
+
+	private static class CrashReportingTree extends Timber.Tree {
+		@Override
+		protected void log(int priority, String tag, String message, Throwable t) {
+			if (priority == Log.VERBOSE || priority == Log.DEBUG) {
+				return;
+			}
+
+//			FakeCrashLibrary.log(priority, tag, message);
+
+			if (t != null) {
+				if (priority == Log.ERROR) {
+//					FakeCrashLibrary.logError(t);
+				} else if (priority == Log.WARN) {
+//					FakeCrashLibrary.logWarning(t);
+				}
+			}
+		}
+	}
+
 }

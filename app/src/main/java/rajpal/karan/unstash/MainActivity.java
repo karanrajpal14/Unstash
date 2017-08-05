67bb64769c3c4a178b6636304267411fae4f1500
Karan M Rajpal <rajpal.karan73@gmail.com>
2017-08-05 23:46:49 +0530

    Refactored some code

--- app/src/main/java/rajpal/karan/unstash/MainActivity.java
+++ app/src/main/java/rajpal/karan/unstash/MainActivity.java
@@ -21,29 +21,35 @@ import net.dean.jraw.managers.AccountManager;
 import net.dean.jraw.models.Contribution;
 import net.dean.jraw.models.Listing;
 import net.dean.jraw.models.Submission;
+import net.dean.jraw.paginators.TimePeriod;
 import net.dean.jraw.paginators.UserContributionPaginator;
+
+import java.util.Date;
 
 import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
 	public static final String TAG = MainActivity.class.getSimpleName();
-	String id = "15lrh7";
+	private RedditClient redditClient;
+	private AccountManager accountManager;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		redditClient = AuthenticationManager.get().getRedditClient();
+		accountManager = new AccountManager(redditClient);
 	}
 
 	public void login(View view) { startActivity(new Intent(this, LoginActivity.class)); }
 	public void userInfo(View view) { startActivity(new Intent(this, UserInfoActivity.class)); }
+
 	public void savePost(View view) {
 		new AsyncTask<Void, Void, Void>(){
 			@Override
 			protected Void doInBackground(Void... voids) {
-				RedditClient reddit = AuthenticationManager.get().getRedditClient();
-				AccountManager accountManager = new AccountManager(reddit);
-				Submission submission = reddit.getSubmission(id);
+				String id = "15lrh7";
+				Submission submission = redditClient.getSubmission(id);
 				try {
 					accountManager.save(submission);
 				} catch (ApiException e) {
@@ -59,9 +65,8 @@ public class MainActivity extends AppCompatActivity {
 		new AsyncTask<Void, Void, Void>(){
 			@Override
 			protected Void doInBackground(Void... voids) {
-				RedditClient reddit = AuthenticationManager.get().getRedditClient();
-				AccountManager accountManager = new AccountManager(reddit);
-				Submission submission = reddit.getSubmission(id);
+				String id = "15lrh7";
+				Submission submission = redditClient.getSubmission(id);
 				try {
 					accountManager.unsave(submission);
 				} catch (ApiException e) {
@@ -71,8 +76,24 @@ public class MainActivity extends AppCompatActivity {
 			}
 		}.execute();
 	}
-	public void getSavedPosts(View view) {
-		Timber.d("SAved posts");
+
+	public void fetchMetadataFromIDs(String id) {
+		Submission submission = redditClient.getSubmission(id);
+		String author = submission.getAuthor();
+		Date created = submission.getCreated();
+		String domain = submission.getDomain();
+		String permalink = submission.getPermalink();
+		Submission.PostHint postHint = submission.getPostHint();
+		Integer score = submission.getScore();
+		String subredditName = submission.getSubredditName();
+		String title = submission.getTitle();
+		String url = submission.getUrl();
+		Boolean nsfw = submission.isNsfw();
+		Boolean saved = submission.isSaved();
+	}
+
+	public void getSavedPostIDs(View view) {
+		Timber.d("Saved posts");
 		new AsyncTask<Object, Object, UserContributionPaginator>(){
 
 			@Override
@@ -80,32 +101,18 @@ public class MainActivity extends AppCompatActivity {
 				RedditClient redditClient = AuthenticationManager.get().getRedditClient();
 				UserContributionPaginator saved = new UserContributionPaginator(redditClient,"saved",redditClient.me().getFullName());
 				Timber.d(String.valueOf(saved.getTimePeriod()));
-				/*saved.setTimePeriod(TimePeriod.MONTH);
-				Timber.d(String.valueOf(saved.getTimePeriod()));*/
+				saved.setTimePeriod(TimePeriod.MONTH);
+				Timber.d(String.valueOf(saved.getTimePeriod()));
+				int i =0;
 				for (Listing<Contribution> items : saved) {
 					for (Contribution item: items){
 						JsonNode dataNode = item.getDataNode();
-						Timber.d("Item1" + dataNode.get("author"));
-						Timber.d("Item2" + dataNode.get("created").getClass());
-						Timber.d("Item3" + dataNode.get("domain"));
-						Timber.d("Item4" + dataNode.get("permalink"));
-						Timber.d("Item5" + dataNode.get("post_hint"));
-						Timber.d("Item6" + dataNode.get("score"));
-						Timber.d("Item7" + dataNode.get("subreddit"));
-						Timber.d("Item8" + dataNode.get("title"));
-						Timber.d("Item9" + dataNode.get("url"));
-						Timber.d("Item10" + dataNode.get("over_18"));
-						Timber.d("Item11" + dataNode.get("saved"));
-						Timber.d("Item11" + dataNode.get("id"));
+						Timber.d("Item " + i + dataNode.get("id"));
+						i++;
 					}
 					saved.next();
 				}
 				return saved;
-			}
-
-			@Override
-			protected void onPostExecute(UserContributionPaginator listings) {
-				super.onPostExecute(listings);
 			}
 		}.execute();
 	}
@@ -121,6 +128,7 @@ public class MainActivity extends AppCompatActivity {
 				break;
 			case NONE:
 				Toast.makeText(MainActivity.this, "Log in first", Toast.LENGTH_SHORT).show();
+				startActivity(new Intent(getApplicationContext(), LoginActivity.class));
 				break;
 			case NEED_REFRESH:
 				refreshAccessTokenAsync();

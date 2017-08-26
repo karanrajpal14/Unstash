e4f6e95310b1a5816e9a28274594a6d1afd27cab
Karan M Rajpal <rajpal.karan73@gmail.com>
2017-08-26 23:04:57 +0530

    Added scheduling notification

--- app/src/main/java/rajpal/karan/unstash/Utils.java
+++ app/src/main/java/rajpal/karan/unstash/Utils.java
@@ -10,14 +10,18 @@ import com.firebase.jobdispatcher.FirebaseJobDispatcher;
 import com.firebase.jobdispatcher.GooglePlayDriver;
 import com.firebase.jobdispatcher.Job;
 import com.firebase.jobdispatcher.Lifetime;
+import com.firebase.jobdispatcher.RetryStrategy;
 import com.firebase.jobdispatcher.Trigger;
 
+import java.util.Calendar;
 import java.util.concurrent.TimeUnit;
 
+import timber.log.Timber;
+
 public class Utils {
-    private static final int REMINDER_INTERVAL_MINUTES = 1;
-    private static final int REMINDER_INTERVAL_SECONDS = (int) TimeUnit.MINUTES.toSeconds(REMINDER_INTERVAL_MINUTES);
-    private static final int SYNC_FLEXTIME_SECONDS = REMINDER_INTERVAL_SECONDS;
+
+    private static int REMINDER_INTERVAL_SECONDS;
+    private static final int SYNC_FLEXTIME_SECONDS = 60;
     private static final String REMINDER_JOB_TAG = "read_post_reminder_tag";
     private static boolean initialized;
 
@@ -30,17 +34,32 @@ public class Utils {
         );
     }
 
+    private static void getTime() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DAY_OF_MONTH, 1);
+        calendar.set(Calendar.HOUR_OF_DAY, 9);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+        long tomorrowEpoch = calendar.getTimeInMillis();
+        long secondsLeft = (tomorrowEpoch - System.currentTimeMillis());
+        Timber.d("Job scheduled for " + getRelativeTime(tomorrowEpoch));
+        REMINDER_INTERVAL_SECONDS = (int) TimeUnit.MILLISECONDS.toSeconds(secondsLeft);
+    }
+
     synchronized public static void scheduleReadPostReminder(@NonNull final Context context) {
 
         if (initialized) return;
 
         Driver driver = new GooglePlayDriver(context);
         FirebaseJobDispatcher dispatcher = new FirebaseJobDispatcher(driver);
-
+        getTime();
         Job constrainedReminderJob = dispatcher.newJobBuilder()
                 .setService(TestJobService.class)
                 .setTag(REMINDER_JOB_TAG)
                 .setConstraints(Constraint.DEVICE_CHARGING)
+                .setConstraints(Constraint.ON_UNMETERED_NETWORK)
+                .setRetryStrategy(RetryStrategy.DEFAULT_EXPONENTIAL)
                 .setLifetime(Lifetime.FOREVER)
                 .setRecurring(true)
                 .setTrigger(Trigger.executionWindow(REMINDER_INTERVAL_SECONDS,

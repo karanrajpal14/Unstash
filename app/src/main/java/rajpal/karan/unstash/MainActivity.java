ba300c296d3e658b9cad1c7e5eacaf81b5a7fbf6
Karan M Rajpal <rajpal.karan73@gmail.com>
2017-08-26 17:56:34 +0530

    Implemented smarter syncing using the JobScheduler and IntentService

--- app/src/main/java/rajpal/karan/unstash/MainActivity.java
+++ app/src/main/java/rajpal/karan/unstash/MainActivity.java
@@ -1,7 +1,6 @@
 package rajpal.karan.unstash;
 
 import android.content.BroadcastReceiver;
-import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
@@ -26,7 +25,6 @@ import android.webkit.CookieManager;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.santalu.emptyview.EmptyView;
 
 import net.dean.jraw.RedditClient;
@@ -35,12 +33,6 @@ import net.dean.jraw.auth.AuthenticationState;
 import net.dean.jraw.auth.NoSuchTokenException;
 import net.dean.jraw.http.oauth.Credentials;
 import net.dean.jraw.http.oauth.OAuthException;
-import net.dean.jraw.models.Contribution;
-import net.dean.jraw.models.Listing;
-import net.dean.jraw.models.Submission;
-import net.dean.jraw.paginators.Sorting;
-import net.dean.jraw.paginators.TimePeriod;
-import net.dean.jraw.paginators.UserContributionPaginator;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -116,76 +108,6 @@ public class MainActivity extends AppCompatActivity
 
     }
 
-    public void fetchSavedPosts() {
-        Log.d(TAG, "fetchSavedPosts: Fetching posts");
-        new AsyncTask<Void, Integer, ContentValues>() {
-
-            @Override
-            protected ContentValues doInBackground(Void... voids) {
-                redditClient = AuthenticationManager.get().getRedditClient();
-                UserContributionPaginator saved = new UserContributionPaginator(redditClient, "saved", redditClient.me().getFullName());
-                saved.setTimePeriod(TimePeriod.ALL);
-                saved.setLimit(0);
-                saved.setSorting(Sorting.NEW);
-                ContentValues savedPostValues = null;
-                for (Listing<Contribution> items : saved) {
-                    for (Contribution item : items) {
-                        JsonNode dataNode = item.getDataNode();
-                        String id = dataNode.get("id").asText();
-
-                        try {
-                            Submission submission = redditClient.getSubmission(id);
-
-                            if (submission != null) {
-
-                                savedPostValues = new ContentValues();
-
-                                // Fetching saved post data
-                                String author = submission.getAuthor();
-                                long created_time = submission.getCreated().getTime();
-                                String domain = submission.getDomain();
-                                String permalink = submission.getPermalink();
-                                String postHint = submission.getPostHint().toString();
-                                int score = submission.getScore();
-                                String subredditName = submission.getSubredditName();
-                                String title = submission.getTitle();
-                                String url = submission.getShortURL();
-                                int isNSFW = (submission.isNsfw()) ? 1 : 0;
-                                int isSaved = (submission.isSaved()) ? 1 : 0;
-                                String thumbnailURL = submission.getThumbnail();
-
-                                savedPostValues.put("post_id", id);
-                                savedPostValues.put("title", title);
-                                savedPostValues.put("author", author);
-                                savedPostValues.put("thumbnail_url", thumbnailURL);
-                                savedPostValues.put("created_time", created_time);
-                                savedPostValues.put("subreddit_name", subredditName);
-                                savedPostValues.put("domain", domain);
-                                savedPostValues.put("post_hint", postHint);
-                                savedPostValues.put("permalink", permalink);
-                                savedPostValues.put("url", url);
-                                savedPostValues.put("score", score);
-                                savedPostValues.put("is_nsfw", isNSFW);
-                                savedPostValues.put("is_saved", isSaved);
-
-                                getContentResolver().insert(SavedPostContract.SavedPostEntry.CONTENT_URI, savedPostValues);
-                            }
-                        } catch (Exception e) {
-                            Timber.d(e.getMessage());
-                        }
-                    }
-                    saved.next(true);
-                }
-                return savedPostValues;
-            }
-
-            @Override
-            protected void onPostExecute(ContentValues contentValues) {
-                super.onPostExecute(contentValues);
-            }
-        }.execute();
-    }
-
     @Override
     protected void onResume() {
         super.onResume();
@@ -209,7 +131,7 @@ public class MainActivity extends AppCompatActivity
 
         switch (state) {
             case READY:
-//				launchFetchService();
+				launchFetchService();
                 break;
             case NONE:
                 showEmpty();
@@ -235,7 +157,7 @@ public class MainActivity extends AppCompatActivity
             protected Void doInBackground(Credentials... params) {
                 try {
                     AuthenticationManager.get().refreshAccessToken(LoginActivity.CREDENTIALS);
-                } catch (NoSuchTokenException | OAuthException e) {
+                } catch (NoSuchTokenException | OAuthException | RuntimeException e) {
                     Log.e(TAG, "Could not refresh access token", e);
                 }
                 return null;
@@ -243,7 +165,7 @@ public class MainActivity extends AppCompatActivity
 
             @Override
             protected void onPostExecute(Void v) {
-//				launchFetchService();
+				launchFetchService();
             }
         }.execute();
     }
@@ -296,7 +218,7 @@ public class MainActivity extends AppCompatActivity
 
         switch (id) {
             case R.id.action_fetch:
-                fetchSavedPosts();
+                launchFetchService();
                 return true;
             case R.id.action_logout:
                 Timber.d("Action logout");
@@ -344,7 +266,7 @@ public class MainActivity extends AppCompatActivity
 
     public void launchFetchService() {
         Intent i = new Intent(this, UnstashFetchService.class);
-        i.putExtra("foo", "bar");
+        i.setAction(UnstashFetchService.ACTION_START_FETCH_SERVICE);
         startService(i);
     }
 }

1d9fc4015857380a1a7901b681036a7f26176dd0
Karan M Rajpal <rajpal.karan73@gmail.com>
2017-09-01 04:14:33 +0530

    Added logging

--- app/src/main/java/rajpal/karan/unstash/StateAwareRecyclerView.java
+++ app/src/main/java/rajpal/karan/unstash/StateAwareRecyclerView.java
@@ -6,11 +6,15 @@ import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
 import android.view.View;
 
+import timber.log.Timber;
+
 public class StateAwareRecyclerView extends RecyclerView {
 
+    private View emptyView;
     final private AdapterDataObserver observer = new AdapterDataObserver() {
         @Override
         public void onChanged() {
+            Timber.d("onChanged called");
             checkIfEmpty();
         }
 
@@ -26,11 +30,13 @@ public class StateAwareRecyclerView extends RecyclerView {
 
         @Override
         public void onItemRangeInserted(int positionStart, int itemCount) {
+            Timber.d("onIRI called");
             checkIfEmpty();
         }
 
         @Override
         public void onItemRangeRemoved(int positionStart, int itemCount) {
+            Timber.d("onIRR called");
             checkIfEmpty();
         }
 
@@ -39,7 +45,6 @@ public class StateAwareRecyclerView extends RecyclerView {
             super.onItemRangeMoved(fromPosition, toPosition, itemCount);
         }
     };
-    private View emptyView;
 
     public StateAwareRecyclerView(Context context) {
         super(context);
@@ -54,16 +59,29 @@ public class StateAwareRecyclerView extends RecyclerView {
     }
 
     void checkIfEmpty() {
+        if (emptyView == null) {
+            Timber.d("EmptyView null");
+        }
+        if (getAdapter() == null) {
+            Timber.d("adapter null");
+        }
         if (emptyView != null && getAdapter() != null) {
             final boolean emptyViewVisible = getAdapter().getItemCount() == 0;
-            emptyView.setVisibility(emptyViewVisible ? VISIBLE : GONE);
-            setVisibility(emptyViewVisible ? GONE : VISIBLE);
+            Timber.d("EmptyViewVisibility " + emptyViewVisible);
+            int vis1 = emptyViewVisible ? VISIBLE : GONE;
+            Timber.d("vis1 " + vis1);
+            emptyView.setVisibility(vis1);
+            int vis2 = emptyViewVisible ? GONE : VISIBLE;
+            Timber.d("vis2 " + vis2);
+            setVisibility(vis2);
         }
     }
 
     @Override
     public void setAdapter(Adapter adapter) {
+        Timber.d("Setting new adapter " + adapter);
         final Adapter oldAdapter = getAdapter();
+        Timber.d("old adapter " + oldAdapter);
         if (oldAdapter != null) {
             oldAdapter.unregisterAdapterDataObserver(observer);
             super.setAdapter(adapter);
@@ -75,6 +93,7 @@ public class StateAwareRecyclerView extends RecyclerView {
     }
 
     public void setEmptyView(View emptyView) {
+        Timber.d("Setting empty view");
         this.emptyView = emptyView;
         checkIfEmpty();
     }

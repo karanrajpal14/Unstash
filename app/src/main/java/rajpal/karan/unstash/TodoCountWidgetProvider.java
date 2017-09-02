6183132a00f64d8bb11af994710db75b3586ccc5
Karan M Rajpal <rajpal.karan73@gmail.com>
2017-09-02 21:58:10 +0530

    Implemented a todo count widget
    Also, fixed issue with widget not updating upon content provider change

--- app/src/main/java/rajpal/karan/unstash/TodoCountWidgetProvider.java
+++ app/src/main/java/rajpal/karan/unstash/TodoCountWidgetProvider.java
@@ -12,6 +12,8 @@ import timber.log.Timber;
 
 public class TodoCountWidgetProvider extends AppWidgetProvider {
 
+    static final int todoWidgetTVID = R.id.todo_widget_textview;
+    static final int doneWidgetTVID = R.id.done_widget_textview;
     private static final String REFRESH_ACTION = AppWidgetManager.ACTION_APPWIDGET_UPDATE;
 
     public static void refreshWidgetBroadcast(Context context) {
@@ -21,16 +23,68 @@ public class TodoCountWidgetProvider extends AppWidgetProvider {
         context.sendBroadcast(intent);
     }
 
+    private static RemoteViews setIntents(RemoteViews rm, Context context) {
+
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+        rm.setOnClickPendingIntent(todoWidgetTVID, pendingIntent);
+        rm.setOnClickPendingIntent(doneWidgetTVID, pendingIntent);
+
+        return rm;
+    }
+
+    public int[] getCounts(Context context) {
+        int todoCount = 0;
+        int doneCount = 0;
+        int[] counts = new int[2];
+
+        try {
+
+            todoCount = context.getContentResolver().query(
+                    SavedPostContract.SavedPostEntry.CONTENT_URI,
+                    null,
+                    SavedPostContract.SavedPostEntry.COLUMN_IS_SAVED + " = 1",
+                    null,
+                    null
+            ).getCount();
+
+            doneCount = context.getContentResolver().query(
+                    SavedPostContract.SavedPostEntry.CONTENT_URI,
+                    null,
+                    SavedPostContract.SavedPostEntry.COLUMN_IS_SAVED + " = 0",
+                    null,
+                    null
+            ).getCount();
+
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        } finally {
+            counts[0] = todoCount;
+            counts[1] = doneCount;
+        }
+        return counts;
+    }
+
     @Override
     public void onReceive(Context context, Intent intent) {
         final String action = intent.getAction();
 
         if (action.equals(REFRESH_ACTION)) {
             Timber.d("Refreshing widget on CP change");
-            AppWidgetManager manager = AppWidgetManager.getInstance(context);
-            ComponentName name = new ComponentName(context, SavedPostProvider.class);
-            manager.notifyAppWidgetViewDataChanged(manager.getAppWidgetIds(name), R.id.todo_widget_textview);
-            manager.notifyAppWidgetViewDataChanged(manager.getAppWidgetIds(name), R.id.done_widget_textview);
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_todo_count);
+            views = setIntents(views, context);
+            int[] counts = getCounts(context);
+            views.setTextViewText(
+                    todoWidgetTVID,
+                    context.getResources().getString(R.string.widget_todo_string, counts[0])
+            );
+            views.setTextViewText(
+                    doneWidgetTVID,
+                    context.getResources().getString(R.string.widget_done_string, counts[1])
+            );
+            ComponentName name = new ComponentName(context, TodoCountWidgetProvider.class);
+            AppWidgetManager.getInstance(context).updateAppWidget(name, views);
         }
     }
 
@@ -38,49 +92,17 @@ public class TodoCountWidgetProvider extends AppWidgetProvider {
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         for (int appWidgetId : appWidgetIds) {
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_todo_count);
+            views = setIntents(views, context);
 
-            int todoWidgetTVID = R.id.todo_widget_textview;
-            int doneWidgetTVID = R.id.done_widget_textview;
-
-            Intent intent = new Intent(context, MainActivity.class);
-            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
-
-            views.setOnClickPendingIntent(todoWidgetTVID, pendingIntent);
-            views.setOnClickPendingIntent(doneWidgetTVID, pendingIntent);
-
-            int todoCount;
-            int doneCount;
-
-            try {
-
-                todoCount = context.getContentResolver().query(
-                        SavedPostContract.SavedPostEntry.CONTENT_URI,
-                        null,
-                        SavedPostContract.SavedPostEntry.COLUMN_IS_SAVED + " = 1",
-                        null,
-                        null
-                ).getCount();
-
-                doneCount = context.getContentResolver().query(
-                        SavedPostContract.SavedPostEntry.CONTENT_URI,
-                        null,
-                        SavedPostContract.SavedPostEntry.COLUMN_IS_SAVED + " = 0",
-                        null,
-                        null
-                ).getCount();
-
-            } catch (NullPointerException e) {
-                todoCount = 0;
-                doneCount = 0;
-            }
+            int[] counts = getCounts(context);
 
             views.setTextViewText(
                     todoWidgetTVID,
-                    context.getResources().getString(R.string.widget_todo_string, todoCount)
+                    context.getResources().getString(R.string.widget_todo_string, counts[0])
             );
             views.setTextViewText(
                     doneWidgetTVID,
-                    context.getResources().getString(R.string.widget_done_string, doneCount)
+                    context.getResources().getString(R.string.widget_done_string, counts[1])
             );
 
             appWidgetManager.updateAppWidget(appWidgetId, views);

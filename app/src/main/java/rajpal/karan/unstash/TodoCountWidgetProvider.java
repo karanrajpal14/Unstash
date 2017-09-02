36fb602f4b63d0b492180c891e5211e8c8154691
Karan M Rajpal <rajpal.karan73@gmail.com>
2017-09-02 19:14:46 +0530

    Added basic widget

---
+++ app/src/main/java/rajpal/karan/unstash/TodoCountWidgetProvider.java
@@ -0,0 +1,65 @@
+package rajpal.karan.unstash;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+public class TodoCountWidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (int appWidgetId : appWidgetIds) {
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_todo_count);
+
+            int todoWidgetTVID = R.id.todo_widget_textview;
+            int doneWidgetTVID = R.id.done_widget_textview;
+
+            Intent intent = new Intent(context, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+            views.setOnClickPendingIntent(todoWidgetTVID, pendingIntent);
+            views.setOnClickPendingIntent(doneWidgetTVID, pendingIntent);
+
+            int todoCount;
+            int doneCount;
+
+            try {
+
+                todoCount = context.getContentResolver().query(
+                        SavedPostContract.SavedPostEntry.CONTENT_URI,
+                        null,
+                        SavedPostContract.SavedPostEntry.COLUMN_IS_SAVED + " = 1",
+                        null,
+                        null
+                ).getCount();
+
+                doneCount = context.getContentResolver().query(
+                        SavedPostContract.SavedPostEntry.CONTENT_URI,
+                        null,
+                        SavedPostContract.SavedPostEntry.COLUMN_IS_SAVED + " = 0",
+                        null,
+                        null
+                ).getCount();
+
+            } catch (NullPointerException e) {
+                todoCount = 0;
+                doneCount = 0;
+            }
+
+            views.setTextViewText(
+                    todoWidgetTVID,
+                    context.getResources().getString(R.string.widget_todo_string, todoCount)
+            );
+            views.setTextViewText(
+                    doneWidgetTVID,
+                    context.getResources().getString(R.string.widget_done_string, doneCount)
+            );
+
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+
+    }
+}
